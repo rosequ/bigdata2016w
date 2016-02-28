090b63b0eec5c3d0922d1d94523b12b7f9c94b07
Qian Xin <xeniaqian94@gmail.com>
2016-02-28 11:50:21 -0500

    update

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/xeniaqian94/assignment5/Q1.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/xeniaqian94/assignment5/Q1.scala
@@ -8,6 +8,8 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
 import org.apache.spark.Partitioner
+import java.util.StringTokenizer
+import scala.collection.JavaConverters._
 
 class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
   mainOptions = Seq(input, date)
@@ -16,58 +18,63 @@ class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
 }
 class MyPartitioner(numOfPar: Int) extends Partitioner {
   def numPartitions: Int = numOfPar
-  def getPartition(key:Any): Int = {
-    val k=key.asInstanceOf[(String,String)]
+  def getPartition(key: Any): Int = {
+    val k = key.asInstanceOf[(String, String)]
     ((k._1.hashCode() & Integer.MAX_VALUE) % numPartitions)
   }
 }
 
-object Q1 extends Tokenizer{
+object Q1 extends Tokenizer {
   val log = Logger.getLogger(getClass().getName())
- 
 
   def main(argv: Array[String]) {
     val args = new Conf(argv)
-    
+
     BasicConfigurator.configure();
 
     log.info("Input: " + args.input())
     log.info("Date: " + args.date())
+    println("hello world")
 
     val conf = new SparkConf().setAppName("Q1")
     System.setProperty("hadoop.home.dir", "/");
+
     val sc = new SparkContext(conf)
 
-//    val outputDir = new Path(args.output())
-//    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+    //    val outputDir = new Path(args.output())
+    //    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
     //TO DO
     val textFile = sc.textFile(args.input())
     val counts = textFile
       .flatMap(line => {
         val tokens = line.split("|").toList
-        log.info(tokens(10)+" "+tokens(11)+" "+tokens(12))
-        if (tokens.length > 1) tokens.sliding(2).flatMap(p => List((p(0),p(1)),(p(0),"*"))).toList else List()
+        
+        log.info("hello world")
+        log.info(tokens(10) + " " + tokens(11) + " " + tokens(12))
+        
+        tokens.toIterator
+//        if (tokens.length > 1) tokens.sliding(2).flatMap(p => List((p(0), p(1)), (p(0), "*"))).toList else List()
       })
-//      .map(bigram => (bigram, 1))
-//      .reduceByKey(_ + _)
-//      .repartitionAndSortWithinPartitions(new MyPartitioner(args.reducers()))
-//      .mapPartitions(iter=>{
-//        var marginal=1
-//        var freq=List[((String,String),Float)]()
-//        while (iter.hasNext){
-//          val x=iter.next;
-//          if (x._1._2.equals("*"))
-//            marginal=x._2 
-//          else
-//            freq=freq.::((x._1,(1.0f*x._2/marginal)))
-//        }
-//        log.info("Freq.length="+freq.length)
-//        freq.toIterator
-//      })
-//      .cache//      .sortByKey()
-//      .saveAsTextFile(args.output())
-      
+    //      .map(bigram => (bigram, 1))
+    //      .reduceByKey(_ + _)
+    //      .repartitionAndSortWithinPartitions(new MyPartitioner(args.reducers()))
+    //      .mapPartitions(iter=>{
+    //        var marginal=1
+    //        var freq=List[((String,String),Float)]()
+    //        while (iter.hasNext){
+    //          val x=iter.next;
+    //          if (x._1._2.equals("*"))
+    //            marginal=x._2 
+    //          else
+    //            freq=freq.::((x._1,(1.0f*x._2/marginal)))
+    //        }
+    //        log.info("Freq.length="+freq.length)
+    //        freq.toIterator
+    //      })
+    //      .cache//      .sortByKey()
+    //      .saveAsTextFile(args.output())
+
   }
-  
+
 }
