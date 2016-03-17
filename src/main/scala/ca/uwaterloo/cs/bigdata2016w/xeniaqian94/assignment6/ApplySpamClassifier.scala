620ff200d21c1e89d4880b4834d7503c2164caa0
Qian Xin <xeniaqian94@gmail.com>
2016-03-17 19:03:30 -0400

    update

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/xeniaqian94/assignment6/ApplySpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/xeniaqian94/assignment6/ApplySpamClassifier.scala
@@ -27,41 +27,46 @@ object ApplySpamClassifier extends Tokenizer {
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
     val modelFile = sc.textFile(args.model())
-    val w = modelFile.map(line => (line.split("[,()]")(1).toInt, line.split("[,()]")(2).toDouble))
-//    .collectAsMap
+    val w = modelFile.map(line => (line.split("[,()]")(1).toInt, line.split("[,()]")(2).toDouble)).collectAsMap
     
     
     def spamminess(features: Array[Int]): Double = {
       var score = 0d
-      features.foreach(f => score+=w.lookup(f).head)
-//          if (w.contains(f)) score += w(f))
+      features.foreach(f => if (w.contains(f)) score += w(f))
       score
     }
     
     //TO DO
-    val textFile = sc.textFile(args.input());
-    val testLabel = textFile.map(line => {
+//    val textFile = sc.textFile(args.input());
+    val testLabel = sc.textFile(args.input()).map(line => {
       val instanceArray = line.split(" ")
       val docid = instanceArray(0)
       //      var isSpam = 0
       //      if (instanceArray(1).equals("spam")) {
       //        isSpam = 1
       //      }
-      val isSpam = instanceArray(1)
-      val features = instanceArray.slice(2, instanceArray.length).map { featureIndex => featureIndex.toInt }
+      val isSpamlabel = instanceArray(1)
+      val features = instanceArray.slice(2, instanceArray.length).map{ featureIndex => featureIndex.toInt }
       // Parse input
       // ..
-      (docid, isSpam, features)
-    }).map(instance => {
-      val docid = instance._1
-      val isSpamlabel = instance._2
-      val spamScore = spamminess(instance._3)
+      val spamScore = spamminess(features)
       var isSpamJudge = "spam"
       if (!(spamScore > 0)) {
         isSpamJudge = "ham"
       }
       (docid, isSpamlabel, spamScore, isSpamJudge)
+//      (docid, isSpam, features)
     })
+//    .map(instance => {
+//      val docid = instance._1
+//      val isSpamlabel = instance._2
+//      val spamScore = spamminess(instance._3)
+//      var isSpamJudge = "spam"
+//      if (!(spamScore > 0)) {
+//        isSpamJudge = "ham"
+//      }
+//      (docid, isSpamlabel, spamScore, isSpamJudge)
+//    })
     testLabel.saveAsTextFile(args.output());
 
   }
