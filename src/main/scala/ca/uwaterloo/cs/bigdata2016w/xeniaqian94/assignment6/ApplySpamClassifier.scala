29eca88611e97a9f260cd47ea83378e0970bcd65
Qian Xin <xeniaqian94@gmail.com>
2016-03-17 19:37:15 -0400

    update

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/xeniaqian94/assignment6/ApplySpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/xeniaqian94/assignment6/ApplySpamClassifier.scala
@@ -67,7 +67,7 @@ object ApplySpamClassifier extends Tokenizer {
 //      }
 //      (docid, isSpamlabel, spamScore, isSpamJudge)
 //    })
-    .repartition(20).saveAsTextFile(args.output())
+    .repartition(200).saveAsTextFile(args.output())
 
   }
 
